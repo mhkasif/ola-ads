a23a4c5561f7f962f5be8c415ee88a7c88a0305c
mhkasif <mhkasif97@gmail.com>
2023-05-31 07:02:43 +0500

    mmkv removed

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,28 +1,13 @@
-// import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {combineReducers} from 'redux';
 import {persistReducer} from 'redux-persist';
 import thunk from 'redux-thunk';
 import AuthReducer from './authSlice/authSlice';
 import AdsReducer from './adsActions/adsSlice';
-import {MMKV} from 'react-native-mmkv';
-export const storage = new MMKV();
-export const reduxStorage = {
-  setItem: (key, value) => {
-    storage.set(key, value)
-    return Promise.resolve(true)
-  },
-  getItem: (key) => {
-    const value = storage.getString(key)
-    return Promise.resolve(value)
-  },
-  removeItem: (key) => {
-    storage.delete(key)
-    return Promise.resolve()
-  },
-}
+
 const persistConfig = {
   key: 'root',
-  storage:reduxStorage,
+  storage: AsyncStorage,
   whitelist: ["auth"], // persist both auth and counter slices
 };
 const rootReducer = combineReducers({
