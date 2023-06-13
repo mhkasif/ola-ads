3f02ca7084c57964b88171c38c8fc85560394465
mhkasif <mhkasif97@gmail.com>
2023-06-13 09:24:11 +0500

    app icon added

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,11 +1,12 @@
-import {NativeBaseProvider, Text} from 'native-base';
+import { NativeBaseProvider } from 'native-base';
 import React from 'react';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {StatusBar} from 'react-native';
-import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react';
-import {persistor, store} from 'redux/store';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from 'redux/store';
+import SplashScreen from 'screens/SplashScreen/SplashScreen';
 import theme from '../../theme';
 
 const config = {
@@ -24,9 +25,7 @@ const AppContainer = props => {
       <NativeBaseProvider theme={theme} config={config}>
         <NavigationContainer>
           <Provider store={store}>
-            <PersistGate
-              loading={<Text>Loading...</Text>}
-              persistor={persistor}>
+            <PersistGate loading={<SplashScreen />} persistor={persistor}>
               {/* <LinearGradient colors={['#FF416C', '#FF4B2B']} style={{flex: 1}}> */}
               {props.children}
             </PersistGate>
